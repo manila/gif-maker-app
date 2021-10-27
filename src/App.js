6e548b5157c312dc49d8cf9fcf15eecc8c788745
manila <manila@users.noreply.github.com>
2021-10-27 18:06:13 -0400

    Initial layout and FFmpeg testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
+import {createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import fs from 'fs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [file, setFile] = useState();
+	const [fileArrayBuffer, setFileArrayBuffer] = useState();
+
+	useEffect(() => {
+
+		if (file) {
+		const ffmpeg = createFFmpeg({ 
+			corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js',	
+			log: true 
+		});
+
+		(async () => {
+			await ffmpeg.load();
+			ffmpeg.FS('writeFile', 'test.avi', await fetchFile(file));
+			await ffmpeg.run('-i', 'test.avi', 'test.mp4');
+			await fs.promises.writeFile('./test.mp4', ffmpeg.FS('readfile', 'test.mp4'));
+			process.exit(0);
+		})();
+			console.log("Hello");
+		}
+		
+		
+	}, [file])
+
+	const fileSelected = (event) => {
+		setFile(event.target.files[0]);
+	}
+
+	return (
+		<div className="content">
+			<Header/>
+			<UploadArea inputEventHandler={ fileSelected } />
+			<PreviewArea/>
+	 		<Controls/>
+	  		<Footer/>
+		</div>
+	);
+}
+
+const Header = () => {
+	return (
+		<>
+			<header>
+				<h1>smaller.pics</h1>
+			</header>
+		</>
+	)
+}
+
+const UploadArea = (props) => {
+	return (
+		<>
+			<label className="upload-area">
+				<input type="file" name="upload" accept="video/*" onInput={ props.inputEventHandler }/>
+			</label>
+		</>
+	)
+}
+
+const PreviewArea = () => {
+	return (
+		<></>
+	)
+}
+
+const Controls = () => {
+	return (
+		<>
+			<form>
+				<button className="download-button">DOWNLOAD</button>
+			</form>
+		</>
+	)
+}
+
+const Footer = () => {
+	return (
+		<>
+			<footer>{ "made with 💜 by manila" }</footer>
+		</>
+	)
 }
 
 export default App;
